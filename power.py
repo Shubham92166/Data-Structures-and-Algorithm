a4be65c1ba2fd0bd0b95f4be0b30f5f231991b9e
Shubham92166 <42581060+Shubham92166@users.noreply.github.com>
2021-05-14 01:28:27 +0530

    Added GCD program and minor bug fixes

--- power.py
+++ power.py
@@ -1,10 +1,17 @@
-def power(base, exp):
-    assert exp>=0 and int(exp)==exp,"Exponent cannot be a float or negative value "
-    if(exp==0):
-        return 1
-    elif(exp==1):
-        return base
-    else:
-        return base*power(base,exp-1)
-base, exp=map(int,input("Enter base and exponent:").split())
+try:
+    def power(base, exp):
+        if(exp<0):
+            print("Exponent cannot be negative..")
+            exit(0)
+        if(exp==0):
+            return 1
+        elif(exp==1):
+            return base
+        else:
+            return base*power(base,exp-1)
+
+    base, exp=map(int,input("Enter base and exponent:").split())
+except:
+    print("Number(s) should be integer only..")
+    exit(0)
 print(power(base,exp))
--- sumOfDigits.py
+++ sumOfDigits.py
@@ -1,8 +1,12 @@
 def sumOfDigits(n):
-    assert n>=0 and int(n)==n," Number cannot be negative"
+    assert n>0, "Number cannot be negative"
     if(n==0):
         return 0
     else:
         return int(n%10)+sumOfDigits(int(n/10))
-n=int(input("Enter any number:"))
+try:
+    n=int(input("Enter any number:"))
+except:
+    print("Number should be positive integer only...")
+    exit(0)
 print(sumOfDigits(n))
