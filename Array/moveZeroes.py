be0c089cffe13fef6f93343bf83c9d4d2f32d799
shubham92166 <shubham92166@gmail.com>
2022-01-28 20:52:52 +0530

    Made changes

--- Array/moveZeroes.py
+++ Array/moveZeroes.py
@@ -9,10 +9,9 @@ def moveZeroesToTheEnd(nums):
     return nums
 '''
 Approach: k pointer will always point to a zero value and when i pointer points to non zero value then we swap both the values.
-Complexity: Time-O(N), Space- O(1)
-'''
 
-'''
+Complexity: Time-O(N), Space- O(1)
+
 Test Cases:
 nums = [0,1,0,3,12]
 nums = [0]
