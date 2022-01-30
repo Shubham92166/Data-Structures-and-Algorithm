9ce2b8af3e50a34da4ae8e883c8f9899b007641f
shubham92166 <shubham92166@gmail.com>
2022-01-30 20:40:31 +0530

    Added isValidPerfectSquare code

---
+++ Array/isValidPerfectSquare.py
@@ -0,0 +1,22 @@
+def isPerfectSquare(num: int):
+        l, r = 1, num
+        while(l<=r):
+            mid = (l+r)//2
+            square = mid*mid
+            if(square==num):
+                return True
+            elif(square>num):
+                r = mid-1
+            else:
+                l = mid+1
+        return False
+#Test Cases: 16, 14, 100, 26, 5, 25 
+
+#Complexity:
+#Time: O(log n)
+#Space: O(1) 
+print(isPerfectSquare(16))
+print(isPerfectSquare(14))
+print(isPerfectSquare(100))
+print(isPerfectSquare(26))
+print(isPerfectSquare(5))
