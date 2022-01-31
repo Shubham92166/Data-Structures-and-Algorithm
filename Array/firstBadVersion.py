db9798dd21a80b043ff6941f6428a6c953e64c52
shubham92166 <shubham92166@gmail.com>
2022-01-31 09:50:56 +0530

    Added firstBadVersion code

---
+++ Array/firstBadVersion.py
@@ -0,0 +1,21 @@
+def firstBadVersion(n):
+    l, r=1,n
+    while(l<=r):
+        mid=(l+r)//2
+
+        #isBadVersion method is implemented by the Leetcode 
+
+        if(isBadVersion(mid)==True):
+            r= mid-1
+        else:
+            l=mid+1
+    return l
+
+#Test Cases: 
+#5, 4
+#2126753390, 1702766719
+#1, 1
+
+#Complexity:
+#Time: O(log n)
+#Space: O(1)
