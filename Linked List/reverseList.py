7a37b001fce7386058041f27b39c155c8c939f59
shubham92166 <shubham92166@gmail.com>
2022-02-11 11:58:56 +0530

    changes

--- Linked List/reverseList.py
+++ Linked List/reverseList.py
@@ -1,5 +1,9 @@
 #Approach-1: Iterative:
 
+from createLinkedList import *
+
+node = Node()
+
 def printReverse(head) :
     prev, cur = None, head
     while(cur):
@@ -7,10 +11,19 @@ def printReverse(head) :
         cur.next = prev
         prev = cur
         cur = next_node
-    while(prev):
-        print(prev.data, end = ' ')
-        prev = prev.next
+    Node().printLL(prev)
 
 #Complexity:
 #Time: O(n)
 #Space: O(1)
+
+#Test Case: [1,2,3,4], [], [1,2,3]
+
+list1 = node.createLL([1,2,3,4])
+print(printReverse(list1))
+
+list2 = node.createLL([])
+print(printReverse(list2))
+
+list3 = node.createLL([1,2,3])
+print(printReverse(list3))
