4feddadd250c053659a5a9bacc2d06a90c0c1c6b
Shubham Singh <shubham92166@gmail.com>
2022-02-25 00:01:09 +0530

    Added and modified some code

--- Linked List/createLinkedList.py
+++ Linked List/createLinkedList.py
@@ -2,25 +2,28 @@ class ListNode:
     def __init__(self, val, Next):
         self.val = val
         self.next = None
-def createLL(nums):
-    head =None
-    cur = None
-    for val in nums:
-        newNode = ListNode(val, None)
-        if head is None:
-            head = newNode
-            cur = newNode
-        else:
-            cur.next = newNode
-            cur = newNode
-    #cur.next = None
-    return head
+class Node:
+    def __init__(self):
+        pass
+    def createLL(self, nums):
+        head =None
+        cur = None
+        for val in nums:
+            newNode = ListNode(val, None)
+            if head is None:
+                head = newNode
+                cur = newNode
+            else:
+                cur.next = newNode
+                cur = newNode
+        #cur.next = None
+        return head
 
-def printLL(head):
-    while(head is not None):
-        print(head.val)
-        head = head.next
+    def printLL(self, head):
+        while(head is not None):
+            print(str(head.val), "->", end = ' ')
+            head = head.next
+        print(None)
 
-head = createLL([1,2,3])
-print(printLL(head))
+
             
