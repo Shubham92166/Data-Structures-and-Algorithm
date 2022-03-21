6549a2ceaaa8287b2e8f206968b31f81743d08a4
Shubham Singh <shubham92166@gmail.com>
2022-03-21 10:27:51 +0530

    Added deleteEveryNNodes code

---
+++ Linked List/deleteEveryNNodes.py
@@ -0,0 +1,12 @@
+from tkinter import N
+from createLinkedList import *
+
+node = Node()
+
+def deleteEveryNNodes(self, head, m, n):
+    cur = head
+    while(cur):
+        retainNodes, deleteNodes = m, n
+        while(retainNodes <= m):
+        
+
