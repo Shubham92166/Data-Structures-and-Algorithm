fca5db6f0f61661792ec45cde3da4c575e0b7b71
Shubham Singh <shubham92166@gmail.com>
2022-07-25 12:37:00 +0530

    Added method2 of findPeakELement code

--- Array/findPeakELement.py
+++ Array/findPeakELement.py
@@ -1,7 +1,6 @@
-from gettext import find
+#Method1:
 
-
-def findPeakElement(arr):
+def findPeakElement1(arr):
     l = 0
     n = len(arr)
     r = n-1
@@ -33,11 +32,37 @@ def findPeakElement(arr):
 #Time: O(log n)
 #Space: O(1)
 
+#Method2:
+
+def findPeakElement2(arr):
+    l, r = 0, len(arr)-1
+    while l <= r:
+        mid = l+(r-l)//2
+        if mid < len(arr)-1 and arr[mid] <= arr[mid+1]:
+            l = mid+1
+        else:
+            r = mid-1
+    return l
+
+#Complexity:
+#Time: O(log n)
+#Space: O(1)
+
 #Test cases:
 #[1,2,3,1], [2,1], [1,2], [2], [1,2,1,3,5,6,4]
 
-print(findPeakElement([1,2,1,3,5,6,4]))
-print(findPeakElement([2]))
-print(findPeakElement([[1,2]]))
-print(findPeakElement([2,1]))
-print(findPeakElement([1,2,3,1],))
+print("Method1")
+#-------------------#
+print(findPeakElement1([1,2,1,3,5,6,4]))
+print(findPeakElement1([2]))
+print(findPeakElement1([[1,2]]))
+print(findPeakElement1([2,1]))
+print(findPeakElement1([1,2,3,1],))
+
+print("Method2")
+#-------------------#
+print(findPeakElement2([1,2,1,3,5,6,4]))
+print(findPeakElement2([2]))
+print(findPeakElement2([[1,2]]))
+print(findPeakElement2([2,1]))
+print(findPeakElement2([1,2,3,1],))
