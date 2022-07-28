c2cadae6528798740e236db6ae9923135638ebbb
Shubham Singh <shubham92166@gmail.com>
2022-07-28 21:56:24 +0530

    Added BalloonEverywhere code

--- String/BalloonEverywhere.py
+++ String/BalloonEverywhere.py
@@ -3,19 +3,19 @@ def maxInstance(s):
     dic = {}
     for i in s:
         dic[i] = dic.get(i, 0)+1
-    possibility = min(dic["b"], dic["a"], dic["n"])
-    if  dic["l"] >= 2*possibility and  dic["o"] >= 2*possibility:
-        return possibility
-    else:
-        return min(dic["l"] // 2, dic["o"] // 2)
+    return min(dic["b"], dic["a"], dic["n"], dic["l"] // 2, dic["o"] // 2)
+    
     
 #Complexity:
 #Time: O(n)
 #Space: O(n)
 
 #Test Cases:
-#aonnollnmlmnoambanoamlmanll, loonbalxballpoon, nlaebolko
+#aonnollnmlmnoambanoamlmanll, loonbalxballpoon, nlaebolko, bnlbllanmbaamnmobbanablboolonlol
 
 print(maxInstance("nlaebolko"))
 print(maxInstance("loonbalxballpoon"))
 print(maxInstance("aonnollnmlmnoambanoamlmanll"))
+print(maxInstance("bnlbllanmbaamnmobbanablboolonlol"))
+
+#Link: https://practice.geeksforgeeks.org/contest/interview-series-58/problems/#
