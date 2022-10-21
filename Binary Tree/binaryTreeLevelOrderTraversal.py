417d1303d0ba40a25dfc8da3217b954beb15deea
Shubham Singh <shubham92166@gmail.com>
2022-10-21 23:54:26 +0530

    Added approach for binaryTreeLevelOrderTraversal code

--- Binary Tree/binaryTreeLevelOrderTraversal.py
+++ Binary Tree/binaryTreeLevelOrderTraversal.py
@@ -1,3 +1,11 @@
+#Approach: 
+#We take a queue to store the nodes. We initially push the root of the tree to queue and None value to mark the end of the 
+#level. We pop from the queue until the queue is not empty. for each node whether we have it left and/or right child. If we have then
+#append it's value to a temporary array which stores the node levelwise. If we find a None value means the level has ended. In this case,
+#we append the tempory aaray to the final answer array which stores the nodes for all levels. We then check if the queue is not empty.
+#If it is not empty then we push None to queue to mark the end of the current level and flush the temporary array. At the end, we return
+#the final answer array.
+
 import queue
 
 from createBinaryTree import *
