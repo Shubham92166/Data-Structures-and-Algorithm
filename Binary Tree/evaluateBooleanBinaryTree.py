79b48611ff663fba20a0d2232e48a98669469812
Shubham Singh <shubham92166@gmail.com>
2022-10-21 09:39:01 +0530

    Added evaluateBooleanBinaryTree code

--- Binary Tree/evaluateBooleanBinaryTree.py
+++ Binary Tree/evaluateBooleanBinaryTree.py
@@ -1,3 +1,13 @@
+#Approach:
+#We do DFS traversal on the given tree. If the current node is the leaf node and it's value is 0 then we return True. If it is the leaf
+#node but it's value is non zero then we return False. +
+#If the current node is a non leaf node and node's value is 2 then we take OR of left subtree and right subtree. Else we take the AND of
+#left subtree and right subtree.
+
+from createBinaryTree import *
+
+tree = BinaryTee()
+
 def evaluateTree(root):
     return helper(root)
 
@@ -17,5 +27,14 @@ def helper(root):
 #Time: O(n)
 #Space: O(h) or O(log n)
 #where h is height of binary tree. Since the given tree is full binary tree so h is log n.
+
+#Test Cases:
+#[2,1,3,-1,-1,0,1,-1,-1,-1,-1], [0,-1,-1]
+
+root = tree.createBinaryTree([2,1,3,-1,-1,0,1,-1,-1,-1,-1])
+print(evaluateTree(root))
+
+root = tree.createBinaryTree([0,-1,-1])
+print(evaluateTree(root))
 
 #Link: https://leetcode.com/problems/evaluate-boolean-binary-tree/
