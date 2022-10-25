c2a970cc6e8320b11e28cb185a540323aab1e495
Shubham Singh <shubham92166@gmail.com>
2022-10-25 09:26:29 +0530

    Added approach for rootEqualsSumOfChildren code

--- Binary Tree/rootEqualsSumOfChildren.py
+++ Binary Tree/rootEqualsSumOfChildren.py
@@ -1,3 +1,13 @@
+'''
+Approach: 
+We check if the roots value is equal to the sum of it's left child's value and right child's value.
+'''
+
+from logging import root
+from createBinaryTree import *
+
+tree = BinaryTee()
+
 def checkTree(root):
     if root.val == root.left.val + root.right.val:
         return True
@@ -9,6 +19,12 @@ def checkTree(root):
 #Space: O(1)
 
 #Test Cases:
-#[10,4,6], [5,3,1]
+#[10,4,6,-1,-1,-1,-1], [5,3,1,-1,-1,-1,-1]
+
+root = tree.createBinaryTree([10,4,6,-1,-1,-1,-1])
+print(checkTree(root))
+
+root = tree.createBinaryTree([5,3,1,-1,-1,-1,-1])
+print(checkTree(root))
 
 #Link: https://leetcode.com/problems/root-equals-sum-of-children/
