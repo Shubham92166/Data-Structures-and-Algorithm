7695e2c8fe46ed7e3ce0b9f03ece581c00281ffb
Shubham Singh <shubham92166@gmail.com>
2022-10-25 09:21:14 +0530

    Added approach for pathSum code

--- Binary Tree/pathSum.py
+++ Binary Tree/pathSum.py
@@ -1,3 +1,16 @@
+'''
+Approach:
+
+We will do DFS traversal on the given tree. Initially we take current sum as 0. While ttraversing, we will add nodes's value to our 
+current sum if that node is non leaf node. If the node is a leaf node then we check if the current sum is equals to the given target.
+If it is equal then we return True. Else we subtract the current node's value from the current sum and return False. We do this process
+for left subtree and right subtree.
+'''
+
+from createBinaryTree import *
+
+tree = BinaryTee()
+
 def hasPathSum(root, targetSum):
     if not root:
         return False
@@ -6,7 +19,9 @@ def hasPathSum(root, targetSum):
 def checkPath(root, sum, targetSum):
     if not root:
         return False
+
     sum += root.val
+
     if not root.left and not root.right:
         if sum == targetSum:
             return True
@@ -25,7 +40,13 @@ def checkPath(root, sum, targetSum):
 #where O(h) can be O(n) in worst case. h is the height and n is the no. of nodes
 
 #Test Cases:
-#[5,4,8,11,null,13,4,7,2,null,null,null,1], 22
-#[1,2,3], 5
+#[5,4,8,11,-1,13,4,7,2,-1,-1,-1,1,-1,-1,-1,-1,-1,-1], 22
+#[1,2,3,-1,-1,-1,-1], 5
+
+root = tree.createBinaryTree([5,4,8,11,-1,13,4,7,2,-1,-1,-1,1,-1,-1,-1,-1,-1,-1])
+print(hasPathSum(root, 22))
+
+root = tree.createBinaryTree([1,2,3,-1,-1,-1,-1])
+print(hasPathSum(root, 5))
 
 #Link: https://leetcode.com/problems/path-sum/
