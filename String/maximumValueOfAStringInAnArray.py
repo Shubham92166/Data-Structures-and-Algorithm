687854830424cc8ddcdde0e34c8c636d5437873c
Shubham Singh <shubham92166@gmail.com>
2022-12-12 23:47:11 +0530

    Added maximumValueOfAStringInAnArray code

--- String/maximumValueOfAStringInAnArray.py
+++ String/maximumValueOfAStringInAnArray.py
@@ -14,4 +14,10 @@ def maximumValue(strs):
 #Space: O(1)
 #Where n is the length of the string
 
+#Test Cases:
+#["alic3","bob","3","4","00000"], ["1","01","001","0001"]
+
+print(maximumValue(["alic3","bob","3","4","00000"]))
+print(maximumValue(["1","01","001","0001"]))
+
 #Link: https://www.geeksforgeeks.org/python-string-isnumeric-method/
