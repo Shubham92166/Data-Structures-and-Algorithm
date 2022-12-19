19050795403f06cb6881f7b32763999a2c525987
Shubham Singh <shubham92166@gmail.com>
2022-12-19 09:37:36 +0530

    Added insertIntoABinarySearchTree code

--- Binary Search Tree/insertIntoABinarySearchTree.py
+++ Binary Search Tree/insertIntoABinarySearchTree.py
@@ -1,5 +1,14 @@
+'''
+Approach: In the BST, we tree to search the node which is to be inserted. When we reach the leaf node and we couldnt find the given node
+then we simply return the new node with the given value. It then gets attached to its correct position.
+'''
+
 import sys
 sys.path.insert(1, "C:\\Users\\Dell\\Desktop\DSA Hands-on\\Data-Structures-and-Algorithms\\Data-Structures-and-Algorithm\\Binary Tree")
+
+from createBinaryTree import *
+
+tree = BinaryTee()
 
 def insertIntoBST(root, val):
     if not root:
@@ -13,9 +22,26 @@ def insertIntoBST(root, val):
         return root
 
 #Complexity:
-#Time: O(H)
-#Space: O(H)
-#where H is the height of the tree. In worst case O(H) can be O(n) and n is the no. of nodes
+#Time: O(h)
+#Space: O(h)
+#where h is the height of the tree. In worst case O(h) can be O(n) and n is the no. of nodes
+
+#Test Cases:
+#[4,2,7,1,3,-1,-1,-1,-1,-1,-1], 5
+#[40,20,60,10,30,50,70,-1,-1,-1,-1,-1,-1,-1,-1], 25
+#[4,2,7,1,3,-1,-1,-1,-1,-1,-1], 5
+
+root = tree.createBinaryTree([4,2,7,1,3,-1,-1,-1,-1,-1,-1])
+newRoot = insertIntoBST(root, 5)
+print(tree.printTree(newRoot))
+
+root = tree.createBinaryTree([40,20,60,10,30,50,70,-1,-1,-1,-1,-1,-1,-1,-1])
+newRoot = insertIntoBST(root, 25)
+print(tree.printTree(newRoot))
+
+root = tree.createBinaryTree([4,2,7,1,3,-1,-1,-1,-1,-1,-1])
+newRoot = insertIntoBST(root, 5)
+print(tree.printTree(newRoot))
 
 #Link: https://leetcode.com/problems/insert-into-a-binary-search-tree/
 
