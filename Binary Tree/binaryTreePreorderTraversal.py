9d4192221f004d1a3e1e8e44ffdeeba9aa12d0b9
Shubham Singh <shubham92166@gmail.com>
2022-12-21 19:33:08 +0530

    Added approach for binaryTreePreorderTraversal code

--- Binary Tree/binaryTreePreorderTraversal.py
+++ Binary Tree/binaryTreePreorderTraversal.py
@@ -1,3 +1,9 @@
+'''
+Approach: 
+We create an empty array/list to store the nodes. We call recursive function, we append the current node's value and then call 
+recursively for left subtree and right subtree. At the end, we return the array/list. 
+'''
+
 from createBinaryTree import *
 
 tree = BinaryTee()
@@ -6,23 +12,31 @@ def preorderTraversal(root):
         ans = []
         preOrder(root, ans)
         return ans
+
 def preOrder(root, ans):
     if not root:
         return []
+
     ans.append(root.val)
+    
     preOrder(root.left, ans)
     preOrder(root.right, ans)
 
 #Complexity:
 #Time: O(n)
-#Space: O(n)
-#where n is the no. of nodes
+#Space: O(h)
+#where n is the no. of nodes and h is the height of the tree. In worst case, h will be n.
 
 #Test Cases:
-#[1,null,2,3], [], [1]
+#[1,-1,2,3,-1,-1,-1], [], [1]
 
-root = tree.createBinaryTree([1,None,2,3])
-print(tree.printTree(root))
+root = tree.createBinaryTree([1,-1,2,3,-1,-1,-1])
+print(preorderTraversal(root))
+
+root = tree.createBinaryTree([-1])
+print(preorderTraversal(root))
+
+root = tree.createBinaryTree([1,-1,-1])
 print(preorderTraversal(root))
 
 #Link: https://leetcode.com/problems/binary-tree-preorder-traversal/
